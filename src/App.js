e953035668167356131178e0621c07702e6b5a9d
atabhi9 <atabhi9@gmail.com>
2025-03-20 19:12:05 +0530

    To do list

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import {useState, useEffect, useMemo, useCallback} from 'react';
 
 function App() {
  const [count, setCount]=useState(0);
+ const [task, setTask]=useState('');
+ const [name, setName]=useState('');
+ const [tasks, setTasks]=useState([]);
+
+ const showTask=()=>{
+  if(task!=='' && name !=''){
+    setTasks([...tasks, {name, task}]);
+    setTask('');
+    setName('');
+  }
+ }
+ const deleteTask=(index)=>{
+  setTasks(tasks.filter((__, i)=>i !==index))
+ }
 
 
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <p1>Count:{count}</p1>
-      <button onClick={()=>setCount(count+1)}>+</button>
+      <input
+      type='text'
+      placeholder='Enter name'
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      />
+
+      <input
+      type='text'
+      placeholder='Enter task'
+      value={task}
+      onChange={(e)=>setTask(e.target.value)}
+      />
+      <button onClick={(showTask)}>Submit</button>
+      <ul>
+        {tasks.map((t, index)=>(
+          <li key={index}>
+            {t.name} : {t.task}  
+            <button onClick={()=>deleteTask(index)}>Delete</button>
+          
+          </li>
+        ))}
+      </ul>
       
     </div>
   );
